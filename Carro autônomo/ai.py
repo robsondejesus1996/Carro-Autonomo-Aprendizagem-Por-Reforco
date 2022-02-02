a7b48651be8f9d23c01aadd8b58b16ab8a471aa6
Robson <robsondejesus1996@hotmail.com>
2022-02-02 19:12:57 -0300

    Definição base organização da rede neural

--- Carro autônomo/ai.py
+++ Carro autônomo/ai.py
@@ -14,3 +14,17 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
+
+
+
+# estrutura da rede
+class Network(nn.Module):
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        
+        
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+        
